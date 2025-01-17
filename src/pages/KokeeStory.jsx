3957f5587b2498ec0be8e3b19b7483468e9cd0a1
simhajin <12shj21@naver.com>
2025-01-17 22:04:04 +0900

    찐막

--- src/pages/KokeeStory.jsx
+++ src/pages/KokeeStory.jsx
@@ -39,7 +39,7 @@ const KokeeStory = () => {
         <div 
           className={style.ParallaxBackground}
           style={{
-            backgroundImage: `url("https://pixabay.com/get/g520d8c08110832d4a3c28d989094bb6fdc755adbd1c3dec7f10c1226d4ab0d62ae0768cd1874e91a80ada33f7009d57e.jpg")`,
+            backgroundImage: `url("https://images.unsplash.com/photo-1461023058943-07fcbe16d735?q=80&w=2069&auto=format&fit=crop")`,
             transform: `translateY(${(scrollPosition - window.innerHeight) * 0.5}px)`
           }}
         >
@@ -55,7 +55,7 @@ const KokeeStory = () => {
         <div 
           className={style.ParallaxBackground}
           style={{
-            backgroundImage: `url("https://pixabay.com/get/geb29643233298c8d856749b4f4041f9ff7b04bf2d2269e4957b26485ca151c63cc1fc4d1f338e81c48856ef1a48f6e68.jpg")`,
+            backgroundImage: `url("https://images.unsplash.com/photo-1507133750040-4a8f57021571?q=80&w=2067&auto=format&fit=crop")`,
             transform: `translateY(${(scrollPosition - window.innerHeight * 2) * 0.5}px)`
           }}
         >
@@ -71,7 +71,7 @@ const KokeeStory = () => {
         <div 
           className={style.ParallaxBackground}
           style={{
-            backgroundImage: `url("https://pixabay.com/get/g5f37bb8e9fa691876a8a800101b34842657ae4e672030ff433d30b2ee6f902d1f89dfb001b2cf13df530e14a50a4d3f7.jpg")`,
+            backgroundImage: `url("https://images.unsplash.com/photo-1445116572660-236099ec97a0?q=80&w=2071&auto=format&fit=crop")`,
             transform: `translateY(${(scrollPosition - window.innerHeight * 3) * 0.5}px)`
           }}
         >
@@ -87,7 +87,7 @@ const KokeeStory = () => {
         <div 
           className={style.ParallaxBackground}
           style={{
-            backgroundImage: `url("https://images.unsplash.com/photo-1507133750040-4a8f57021571?q=80&w=2067&auto=format&fit=crop")`,
+            backgroundImage: `url("https://images.unsplash.com/photo-1511537190424-bbbab87ac5eb?q=80&w=2070&auto=format&fit=crop")`,
             transform: `translateY(${(scrollPosition - window.innerHeight * 4) * 0.5}px)`
           }}
         >
