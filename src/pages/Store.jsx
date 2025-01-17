eb8d9136b466c4f4a414ba5845b5984833b94168
KMS <awdx303@gmail.com>
2025-01-17 23:47:42 +0900

    store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -11,31 +11,31 @@ const Store = () => {
     },
     {
       id: 2,
-      name: "소세지 빵점",
+      name: "구로점",
       address: "서울시 구로구 디지털로 300 (구로동)",
       phone: "02-123-4567",
     },
     {
       id: 3,
-      name: "문익점",
+      name: "구로점",
       address: "서울시 구로구 디지털로 300 (구로동)",
       phone: "02-123-4567",
     },
     {
       id: 4,
-      name: "점점",
+      name: "구로점",
       address: "서울시 구로구 디지털로 300 (구로동)",
       phone: "02-123-4567",
     },
     {
       id: 5,
-      name: "돈점...",
+      name: "구로점",
       address: "서울시 구로구 디지털로 300 (구로동)",
       phone: "02-123-4567",
     },
     {
       id: 6,
-      name: "아점",
+      name: "구로점",
       address: "서울시 구로구 디지털로 300 (구로동)",
       phone: "02-123-4567",
     },
@@ -43,7 +43,17 @@ const Store = () => {
 
   return (
     <div className={style.Container}>
+      {/* 검색창 영역 */}
       <div className={style.MainContent}>
+        <div className={style.SearchContainer}>
+          <input
+            className={style.SearchBar}
+            type="text"
+            placeholder="매장명 또는 주소를 입력하세요."
+          />
+          <button className={style.SearchButton}>◎</button>
+        </div>
+        {/* 상점 목록 영역 */}
         <div className={style.StoreList}>
           {stores.map((store) => (
             <div key={store.id} className={style.StoreItem}>
@@ -55,6 +65,9 @@ const Store = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className={style.map}>
+        <img src="../../public/img/map.png" alt="img!!" />
       </div>
     </div>
   );
